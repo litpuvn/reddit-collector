929bfde8e549979f07dc42d7fa19aa9135a15271
JStuve <stuve.josh@gmail.com>
2018-02-07 15:54:33 -0600

    Simple subreddit comment scrapper

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ reddit = praw.Reddit(client_id= config.client_id,
                      client_secret= config.client_secret,
                      user_agent= config.user_agent)
 
-for submission in reddit.subreddit('learnpython').hot(limit=10):
-    print(submission.title)
+for comment in reddit.subreddit('opiatesrecovery').comments(limit=50):
+    print(comment.body.encode('utf-8'))
